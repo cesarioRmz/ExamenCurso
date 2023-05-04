fa9f4af7ed2fb714b244c90ae15c38f8fee73592
cesarioRmz <>
2023-05-04 10:45:08 -0600

    subiendo repositorio examen

---
+++ src/main/java/com/mx/ExamenCurso/ExamenCursoApplication.java
@@ -0,0 +1,13 @@
+package com.mx.ExamenCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenCursoApplication.class, args);
+	}
+
+}
